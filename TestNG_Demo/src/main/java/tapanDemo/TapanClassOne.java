b42ffd3bc33bb5f3afa550156ed13e562865edb4
tapanbojja23 <rtRonit@9970>
2022-04-28 08:27:03 +0530

    few more comitting changes

--- TestNG_Demo/src/main/java/tapanDemo/TapanClassOne.java
+++ TestNG_Demo/src/main/java/tapanDemo/TapanClassOne.java
@@ -1,4 +1,32 @@
 package tapanDemo;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
+import org.openqa.selenium.WebDriver;
+
 public class TapanClassOne {
+
+    public static WebDriver driver;
+
+
+    public static void main(String[] args) {
+
+        // First option to open chrome browser
+        WebDriverManager driverManager = new ChromeDriverManager();
+        driverManager.setup(); // It will configure the chrome browser on particular machine
+
+        // Second option to open chrome browser
+        WebDriverManager.chromedriver().setup();
+
+
+
+    }
+
+
+
+
+
+
+
+
 }
