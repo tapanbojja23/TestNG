609621a475e8011f165bf85313a7dc529c157a67
tapanbojja23 <rtRonit@9970>
2022-05-16 12:24:28 +0530

    minor changes in  code for ompileTimeException

--- TestNG_Demo/src/main/java/exceptionHandling/CompileTimeException.java
+++ TestNG_Demo/src/main/java/exceptionHandling/CompileTimeException.java
@@ -3,16 +3,18 @@ package exceptionHandling;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
+/*
 public class CompileTimeException extends Exception {
 
-     // final void show()
+
     {
         System.out.println("I am in show method");
     }
 
 }
+*/
 
-class Test extends CompileTimeException
+class Test //extends CompileTimeException
 {
      public static void main(String[] args) throws FileNotFoundException {
          try {
