eb64c42157ff83898991222618be21781a485572
tapanbojja23 <rtRonit@9970>
2022-05-16 12:34:06 +0530

    minor changes in  code for ompileTimeException

--- TestNG_Demo/src/main/java/exceptionHandling/CompileTimeException.java
+++ TestNG_Demo/src/main/java/exceptionHandling/CompileTimeException.java
@@ -18,12 +18,21 @@ class Test //extends CompileTimeException
 {
      public static void main(String[] args) throws FileNotFoundException {
          try {
-             FileInputStream fis = new FileInputStream("c:/java-basic-concepts.txt");
+                       Class.forName("com.mysql.jdbc.Driver");
+         }
+
+        /* try {
+              FileInputStream fis = new FileInputStream("c:/java-basic-concepts.txt");
          }
 
          catch (Exception e)
          {
              System.out.println(e);
+         }*/
+
+         catch (Exception ec)
+         {
+             System.out.println(ec);
          }
 
       //  CompileTimeException ce = new CompileTimeException();
